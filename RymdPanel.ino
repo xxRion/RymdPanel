ff4b5fd908dd715c2567265b26262b05ba0020e5
Mattias Fransson <mattias@webben.nu>
2020-11-07 15:32:15 +0100

    Add start key pins

--- RymdPanel.ino
+++ RymdPanel.ino
@@ -6,6 +6,8 @@ int buttonCount = sizeof(buttonLedPins)/sizeof(buttonLedPins[0]);
 //A10 and A11 accidentaly connected in reverse
 int redPins[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A11, A10 };
 int redCount = sizeof(redPins)/sizeof(redPins[0]);
+int keyStart = 9;
+int keyOn = 8;
 
 void led_blink(int led) {
   digitalWrite(led, HIGH);
@@ -54,6 +56,10 @@ void setup() {
     pinMode(buttonStatePins[thisPin], INPUT_PULLUP);
   }
 
+  // Start key pins
+  pinMode(keyStart, INPUT_PULLUP);
+  pinMode(keyOn, INPUT_PULLUP);
+
   //10 st led i strömbrytare
   for (int thisPin = 0; thisPin < buttonCount; thisPin++) {
     pinMode(buttonLedPins[thisPin], OUTPUT);
@@ -70,4 +76,6 @@ void loop() {
   for (int thisPin = 0; thisPin < buttonCount; thisPin++) {
     if (buttonStates[thisPin] == LOW) system_test();
   }
+  if (digitalRead(keyOn) == LOW) led_blink(redPins[5]);
+  if (digitalRead(keyStart) == LOW) led_blink(redPins[7]);
 }
